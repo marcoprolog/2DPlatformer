b8a2ad2acdfbbbe8b3c6989034ed3057e20d77de
marcoprolog <marco.prolog@gmail.com>
2021-09-27 16:49:17 +0200

    Fixed the death trigger to be a trigger
    The issue was that triggers only work if we have a rigidbody, and we forgot to have that

--- 2DPlatformer/Assets/Scripts/DeathTrigger.cs
+++ 2DPlatformer/Assets/Scripts/DeathTrigger.cs
@@ -5,7 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class DeathTrigger : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
             SceneManager.LoadScene("Platformer");
