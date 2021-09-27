5a60dd6699932f9cc6ef23a79911f94fafa854f2
marcoprolog <marco.prolog@gmail.com>
2021-09-27 16:42:08 +0200

    Code written in class

---
+++ 2DPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 25;
+    float moveInput = 0;
+    public float jumpForce = 10;
+    public bool isGrounded = false;
+    bool jump = false;
+    public GameObject groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        RaycastHit2D hit = Physics2D.Linecast(transform.position, groundCheck.transform.position);
+        if (hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(Vector2.right * moveInput * speed);
+
+        if (jump)
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+            jump = false;
+        }
+    }
+}
